89f35103c18f94dc63e40c8e6631dfc4784b9607
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-02-28 22:12:21 -0300

    Criando botoes de navegacao

--- Abas/Abas/MainPage.xaml.cs
+++ Abas/Abas/MainPage.xaml.cs
@@ -14,5 +14,25 @@ namespace Abas
         {
             InitializeComponent();
         }
+
+        private void btnProximo1_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[1];
+        }
+
+        private void btnProximo2_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage= Children[2];
+        }
+
+        private void btnAnterior2_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage= Children[0];
+        }
+
+        private void btnAnterior3_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage= Children[1];
+        }
     }
 }
